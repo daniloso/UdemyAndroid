ef8e140241cd329ac1d374f7b28748700dba9efd
Danilo <daniloso20@gmail.com>
2021-01-12 08:41:40 -0300

    Aula 1: mostrando msg na tela

--- Agenda/app/src/main/java/alura/com/br/agenda/MainActivity.java
+++ Agenda/app/src/main/java/alura/com/br/agenda/MainActivity.java
@@ -1,8 +1,16 @@
 package alura.com.br.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Danilo o bom!", Toast.LENGTH_LONG).show();
+    }
 }
