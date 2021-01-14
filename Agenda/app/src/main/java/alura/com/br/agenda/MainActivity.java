e788220797bc1412eb3540205c5a5e4e86f7888f
Danilo <daniloso20@gmail.com>
2021-01-13 21:07:28 -0300

    Aula 2: utilizando ListView

--- Agenda/app/src/main/java/alura/com/br/agenda/MainActivity.java
+++ Agenda/app/src/main/java/alura/com/br/agenda/MainActivity.java
@@ -2,6 +2,9 @@ package alura.com.br.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,16 +22,15 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main); // agora com o activity criado na pasta layout
 
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Danilo", "Alex", "Goku")
-        );
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceirooAluno = findViewById(R.id.textView3);
+                Arrays.asList("Danilo", "Alex", "Goku", "Gohan", "Mendigo Voador", "Donald Trunph", "Bolsonaro", "Lacraia", "Zé", "Tio Patinhas"));
 
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceirooAluno.setText(alunos.get(2));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
 
+        /*  o componente "ListView" implenta a lista de alunos no Layout
+            o método "findViewById" serve para buscar o elemento por um critério, no caso aqui o ID
+            apartir da classe "ArrayAdapter" eu posso enviar um contexto (lista alunos) no para o layout ja predefinido do framework android (simple_list_item_1)
+         */
 
     }
 }
